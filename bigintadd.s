7190d40ee697bae03b2fe11037e8b09a69942429
Saleema Diallo <sd0936@princeton.edu>
2025-11-28 13:27:40 -0500

    changes

---
+++ bigintadd.s
@@ -0,0 +1,119 @@
+//--------------------------------------------------------------------
+// bigintadd.s                                                            
+// Author: Saleema & Ludan                                            
+//--------------------------------------------------------------------
+// enum {FALSE, TRUE};
+        .equ    FALSE, 0
+        .equ    TRUE, 1
+
+//----------------------------------------------------------------------
+        .section .rodata
+
+//----------------------------------------------------------------------
+        .section .bss 
+
+//----------------------------------------------------------------------
+        .section .data 
+
+//----------------------------------------------------------------------
+        .section .text
+
+    //------------------------------------------------------------------
+    // Return the larger of lLength1 and lLength2. 
+    //------------------------------------------------------------------
+
+    // Must be a multiple of 16
+    .equ    STACK_BYTECOUNT, 16
+    
+    // Parameter stack offsets
+    .equ LLENGTH1, 8
+    .equ LLENGTH2, 16
+
+    // Local variable stack offsets
+    .equ LLARGER, 24
+
+    .global BigInt_larger
+
+// static long BigInt_larger(long lLength1, long lLength2)
+BigInt_larger:
+        // Prolog
+        sub     sp, sp, STACK_BYTECOUNT
+        str     x30, [sp]   // Save x30
+        // Parameters: long lLength1, long lLength2
+        str x0, [sp, LLENGTH1]    // Save lLength1
+        str x1, [sp, LLENGTH2]    // Save lLength2 
+        // long lLarger;
+        // if (lLength1 <= lLength2) goto else1;
+    
+     
+  
+
+    if (lLength1 <= lLength2) goto else1;
+    lLarger = lLength1;
+    goto endif1;
+    else1:
+        lLarger = lLength2;
+    endif1:
+    return lLarger;
+    
+
+/*--------------------------------------------------------------------*/
+
+/* Assign the sum of oAddend1 and oAddend2 to oSum.  oSum should be
+   distinct from oAddend1 and oAddend2.  Return 0 (FALSE) if an
+   overflow occurred, and 1 (TRUE) otherwise. */
+
+int BigInt_add(BigInt_T oAddend1, BigInt_T oAddend2, BigInt_T oSum)
+{
+   unsigned long ulCarry;
+   unsigned long ulSum;
+   long lIndex;
+   long lSumLength;
+ 
+   /* Determine the larger length. */
+   lSumLength = BigInt_larger(oAddend1->lLength, oAddend2->lLength);
+
+   /* Clear oSum's array if necessary. */
+   if (oSum->lLength <= lSumLength) goto endif1;
+   memset(oSum->aulDigits, 0, MAX_DIGITS * sizeof(unsigned long));
+
+   endif1:
+   /* Perform the addition. */
+   ulCarry = 0;
+   lIndex = 0;
+   loop1:
+   if(lIndex >= lSumLength) goto endloop1;
+   ulSum = ulCarry;
+   ulCarry = 0;
+
+   ulSum += oAddend1->aulDigits[lIndex];
+   if (ulSum >= oAddend1->aulDigits[lIndex]) goto endif2; /* Check for overflow. */
+   ulCarry = 1;
+   
+   endif2:
+   ulSum += oAddend2->aulDigits[lIndex];
+   if (ulSum >= oAddend2->aulDigits[lIndex]) goto endif3;  /* Check for overflow. */
+   ulCarry = 1;
+
+   endif3:
+   oSum->aulDigits[lIndex] = ulSum;
+   lIndex++;
+   goto loop1;
+
+   endloop1:
+   /* Check for a carry out of the last "column" of the addition. */
+   if (ulCarry != 1) goto endif4;
+   
+   if (lSumLength != MAX_DIGITS) goto endif5;
+   return FALSE;
+   
+   endif5:
+   oSum->aulDigits[lSumLength] = 1;
+   lSumLength++;
+   
+   endif4:
+   /* Set the length of the sum. */
+   oSum->lLength = lSumLength;
+
+   return TRUE;
+}
--- bigintaddFlat.c
+++ bigintaddFlat.c
@@ -18,10 +18,12 @@ enum {FALSE, TRUE};
 static long BigInt_larger(long lLength1, long lLength2)
 {
    long lLarger;
-   if (lLength1 > lLength2)
-      lLarger = lLength1;
-   else
+   if (lLength1 <= lLength2) goto else1;
+   lLarger = lLength1;
+   goto endif1;
+   else1:
       lLarger = lLength2;
+   endif1:
    return lLarger;
 }
 
@@ -37,47 +39,49 @@ int BigInt_add(BigInt_T oAddend1, BigInt_T oAddend2, BigInt_T oSum)
    unsigned long ulSum;
    long lIndex;
    long lSumLength;
-
-   assert(oAddend1 != NULL);
-   assert(oAddend2 != NULL);
-   assert(oSum != NULL);
-   assert(oSum != oAddend1);
-   assert(oSum != oAddend2);
  
    /* Determine the larger length. */
    lSumLength = BigInt_larger(oAddend1->lLength, oAddend2->lLength);
 
    /* Clear oSum's array if necessary. */
-   if (oSum->lLength > lSumLength)
-      memset(oSum->aulDigits, 0, MAX_DIGITS * sizeof(unsigned long));
+   if (oSum->lLength <= lSumLength) goto endif1;
+   memset(oSum->aulDigits, 0, MAX_DIGITS * sizeof(unsigned long));
 
+   endif1:
    /* Perform the addition. */
    ulCarry = 0;
-   for (lIndex = 0; lIndex < lSumLength; lIndex++)
-   {
-      ulSum = ulCarry;
-      ulCarry = 0;
+   lIndex = 0;
+   loop1:
+   if(lIndex >= lSumLength) goto endloop1;
+   ulSum = ulCarry;
+   ulCarry = 0;
 
-      ulSum += oAddend1->aulDigits[lIndex];
-      if (ulSum < oAddend1->aulDigits[lIndex]) /* Check for overflow. */
-         ulCarry = 1;
+   ulSum += oAddend1->aulDigits[lIndex];
+   if (ulSum >= oAddend1->aulDigits[lIndex]) goto endif2; /* Check for overflow. */
+   ulCarry = 1;
+   
+   endif2:
+   ulSum += oAddend2->aulDigits[lIndex];
+   if (ulSum >= oAddend2->aulDigits[lIndex]) goto endif3;  /* Check for overflow. */
+   ulCarry = 1;
 
-      ulSum += oAddend2->aulDigits[lIndex];
-      if (ulSum < oAddend2->aulDigits[lIndex]) /* Check for overflow. */
-         ulCarry = 1;
+   endif3:
+   oSum->aulDigits[lIndex] = ulSum;
+   lIndex++;
+   goto loop1;
 
-      oSum->aulDigits[lIndex] = ulSum;
-   }
-
+   endloop1:
    /* Check for a carry out of the last "column" of the addition. */
-   if (ulCarry == 1)
-   {
-      if (lSumLength == MAX_DIGITS)
-         return FALSE;
-      oSum->aulDigits[lSumLength] = 1;
-      lSumLength++;
-   }
-
+   if (ulCarry != 1) goto endif4;
+   
+   if (lSumLength != MAX_DIGITS) goto endif5;
+   return FALSE;
+   
+   endif5:
+   oSum->aulDigits[lSumLength] = 1;
+   lSumLength++;
+   
+   endif4:
    /* Set the length of the sum. */
    oSum->lLength = lSumLength;
 
